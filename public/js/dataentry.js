68babb0d1638d2b4a91c7445e929ca382df32626
Courtenay <hdsheena@users.noreply.github.com>
2024-05-23 13:26:52 -0700

    add toggle switch and js file for data entry

---
+++ public/js/dataentry.js
@@ -0,0 +1,14 @@
+// add listener for toggle id modeSwitch
+const modeSwitch = document.getElementById('modeSwitch');
+const mineshaftTableContainer = document.getElementById('mineshaftTableContainer');
+
+modeSwitch.addEventListener('change', function() {
+ 
+    const mineshaftTableContainer = document.getElementById('mineshaftTableContainer');
+    if (modeSwitch.checked) {
+      mineshaftTableContainer.style.display = 'none';
+    } else {
+      mineshaftTableContainer.style.display = 'block';
+    }
+  }
+);
